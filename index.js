a53390dca4b606994ce04eece008a0edd67b5ca4
unknown <aztoorabally7447@gmail.com>
2022-06-08 11:58:35 +0200

    added functionality

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let sum = " ";
+function build(char) {
+  sum += char;
+  document.getElementById("output").innerHTML = sum;
+}
+function calc(){
+    sum=eval(sum);
+    document.getElementById("output").innerHTML= null;
+    document.getElementById("output").innerHTML = sum;
+   
+}
+function clr(){
+    document.getElementById("output").innerHTML= null;
+     sum = '';
+}
+function chng(col){
+    document.querySelector('#output').style.color='#ff786934';
+}
